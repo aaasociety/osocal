fa9e4a0fdb8ee14fcc1e8ca60527af5a368a1a96
Abdul Karim Kikar <abdu1771@appsodense.dk>
2022-12-02 20:19:03 +0100

    We are getting somewhere
    Here we have a basic curses prompt. We can expand it to include other
    commands.
    
    Signed-off-by: Abdul Karim Kikar <abdu1771@appsodense.dk>

--- calview.py
+++ calview.py
@@ -1,20 +1,37 @@
 #!/bin/python3
 
-import sys
-import src.term as trm
+import sys,curses
+w = curses.initscr()
+w.keypad(True)
+
+def leave():
+    curses.echo()
+    w.keypad(False)
+    curses.nocbreak()
+    curses.endwin()
+    sys.exit(1)
 
 def main():
     try:
-        trm.init()
-        trm.print("Hello World!");
-        print("HELLO WORLD!")
+        w.clear()
+        w.addstr("? for help\n")
+        w.refresh()
+        while 1:
+            w.addstr("> ")
+            cmd = w.getch()
+            w.addstr("\n");
+            if cmd == 113:
+                leave()
+            w.refresh()            
+        
     except KeyboardInterrupt:
-        print(trm.debug())
+        w.addstr("\nAborting...\n")
+        leave()
         sys.exit(1)
 
 if __name__ == "__main__":
     main()
+    
 else:
     print("Do not import this program")
-    leave()
     sys.exit(1)
