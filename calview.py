7f2061157ec6c4822f7ca85d5d54f34c36d3abfc
Abdul Karim Kikar <abdu1771@appsodense.dk>
2022-12-03 12:13:23 +0100

    We are truly getting somewhere
    1. A prompt that is not broken.
    2. A WIP file opening mechanism
    
    All that's left to do is implement the actual calendar format
    
    Signed-off-by: Abdul Karim Kikar <abdu1771@appsodense.dk>

--- calview.py
+++ calview.py
@@ -1,8 +1,10 @@
-#!/bin/python3
+t#!/bin/python3
 
 import sys,curses
 w = curses.initscr()
 w.keypad(True)
+
+debug=True
 
 def leave():
     curses.echo()
@@ -11,17 +13,51 @@ def leave():
     curses.endwin()
     sys.exit(1)
 
+cal = ""
+
+def calOpenPrompt():
+    try:
+        w.clear()
+        w.addstr("Specify which calendar file you want open")
+        w.addstr("\nOr press Enter for the default (cal.txt)")
+        w.refresh()
+        w.keypad(False)
+        while 1:
+            w.addstr("\n> ")
+            cmd = w.getstr()
+            if debug == True:
+                print(str(str(cmd) + "\n"))
+            if cmd == "quit":
+                leave()
+            
+    except KeyboardInterrupt:
+        w.addstr("\nAborting...\n")
+        leave()
+        sys.exit(1)
+    
+
 def main():
     try:
         w.clear()
-        w.addstr("? for help\n")
+        # Calendar view mechanism
+        if cal == "":
+            w.addstr("\n- - - - - - - -")
+            w.addstr("\nNo calendar file open")
+            w.addstr("\nPress \"o\" and specify")
+            w.addstr("\nWhich file to open")
+            w.addstr("\n- - - - - - - -\n")
+        w.addstr("Type ? for help\n")    
         w.refresh()
         while 1:
             w.addstr("> ")
             cmd = w.getch()
+            if debug == True:
+                w.addstr(str("\n" + str(cmd)))
             w.addstr("\n");
             if cmd == 113:
                 leave()
+            if cmd == 111:
+                calOpenPrompt()
             w.refresh()            
         
     except KeyboardInterrupt:
