0c59072251bd7928d1b527894cfe7ba11c5205f4
Abdul Karim Kikar <abdu1771@appsodense.dk>
2022-12-15 11:49:24 +0100

    Ah screw it
    Let's merge viewing *and* editing into the same program.
    
    Signed-off-by: Abdul Karim Kikar <abdu1771@appsodense.dk>

--- calview.py
+++ calview.py
@@ -23,20 +23,55 @@ row = lib.row
 col = lib.col
 ERR = lib.ErrType
 
-def calview(calFile = "cal.txt",month = date.getStartingDate().split("-")[0], year = date.getStartingDate().split("-")[1]):
+def editEntry(day,month,year = lib.year, events = lib.events,calFile = "cal.txt"):
+    w.clear()
+    row = 0
+    col = 0
+    cur = 0
+    if cal.dayHasEvent(day,month,year,events):
+        eventid = cal.getId(day,month,year,events)
+        w.addstr(row,col,"Redigere eksisterende begivenhed")
+        w.addstr(row + 1,0,"Navn: " + str(cal.getName(eventid,events)))
+        w.addstr(row + 2,0,"Sted: " + str(cal.getLocation(eventid,events)))
+        w.addstr(row + 3,0,"Begynder (YYYY-MM-DD-HH-MM): " + str(cal.getStartDate(eventid,events)))
+        w.addstr(row + 4,0,"Slutter (YYYY-MM-DD-HH-MM):" + str(cal.getEndDate(eventid,events)))
+        w.addstr(row + 5,0,"Noter/Andre information: " + str(cal.getNotes(eventid,events)))
+    else:
+        w.addstr(row,col,"Tilsæt nyt begivenhed")
+        w.addstr(row + 1,0,"Navn: ")
+        w.addstr(row + 2,0,"Sted: ")
+        w.addstr(row + 3,0,"Begynder (YYYY-MM-DD-HH-MM): ")
+        w.addstr(row + 4,0,"Slutter (YYYY-MM-DD-HH-MM): ")
+        w.addstr(row + 5,0,"Noter/Andre information: ")
+    while True:
+        cmd = w.getch()
+        # TODO: Implement this
+    calview(calFile,month,year)
+    
+def calview(calFile = "cal.txt",month = int(date.getStartingDate().split("-")[0]), year = int(date.getStartingDate().split("-")[1])):
     try:
+        # Make sure month and years are integers
+        month = int(month)
+        year = int(year)
+        
+        # If calFile is empty then
+        # replace it with the sane default that is "cal.txt"
+        if calFile == "":
+            calFile = "cal.txt"
+        
         # Open the calendar and store the list of events
         events = cal.parse(str(calFile))
         
         lib.year = year # Set global year to starting year
         
-        CellList = term.renderCalendar(month,year)# Render the actual calendar.
+        CellList = term.renderCalendar(str(month),year)# Render the actual calendar.
 
-        lastrow = int(CellList[len(CellList) - 1].split("-")[1]) + 1 # This variable stores the row number that we can print to. This is used to print event info and debug info.
+        lastrow = int(CellList[len(CellList) - 1].split("-")[1]) + 2 # This variable stores the row number that we can print to. This is used to print event info and debug info.
         item = 1 # This variable stores the cell we are currently on.
         curses.curs_set(2) # Set cursor to very visible mode.
         
         wasThereAnEvent = False # A flag to track whether or not an event was displayed, this is used to clear ther terminal at the correct time.
+        w.addstr(lastrow - 1,0,"--")
         while True:
 
             # Calculate the col and row of the cell we want to
@@ -57,19 +92,25 @@ def calview(calFile = "cal.txt",month = date.getStartingDate().split("-")[0], ye
             # Ok so here we display the actual event info, if it exists.
             if cal.dayHasEvent(item,month,year,events):
                 eventid = cal.getId(item,month,year,events)
-                w.addstr(lastrow,0,"Event name: " + str(cal.getName(eventid,events)))
-                w.addstr(lastrow + 1,0,"Location: " + str(cal.getLocation(eventid,events)))
-                w.addstr(lastrow + 2,0,"Starts at: " + date.prettifyDate(str(cal.getStartDate(eventid,events))))
-                w.addstr(lastrow + 3,0,"Ends at: " + date.prettifyDate(str(cal.getEndDate(eventid,events))))
-                w.addstr(lastrow + 4,0,"Notes: " + str(cal.getNotes(eventid,events)))            
+                try:
+                    w.addstr(lastrow,0,"Navn: " + str(cal.getName(eventid,events)))
+                    w.addstr(lastrow + 1,0,"Sted: " + str(cal.getLocation(eventid,events)))
+                    w.addstr(lastrow + 2,0,"Begynder: " + date.prettifyDate(str(cal.getStartDate(eventid,events))))
+                    w.addstr(lastrow + 3,0,"Slutter " + date.prettifyDate(str(cal.getEndDate(eventid,events))))
+                    w.addstr(lastrow + 4,0,"Noter/Andre information: " + str(cal.getNotes(eventid,events)))
+                except curses.error:
+                    pass
                 wasThereAnEvent = True
             elif wasThereAnEvent == True:
                 wasThereAnEvent = False
 
+            # Print the basic calendar help dialog if
+            # we are not on an event
             if wasThereAnEvent == False:
                 w.addstr(lastrow,0,dialog.basiccalhelp)
                 
             # Move the cursor to the cell that the user has selected
+            # And then wait for a command
             w.move(rowofitem,colofitem)
             cmd = w.getch()
             if term.compKey(cmd,curses.KEY_RIGHT,261): # Right arrow key
@@ -84,6 +125,22 @@ def calview(calFile = "cal.txt",month = date.getStartingDate().split("-")[0], ye
                     item -= 1
                 else:
                     item = 1
+            elif term.compKey(cmd,curses.KEY_UP,259): # Up arrow key
+                if month >= 12:
+                    month = 1
+                    year += 1
+                else:
+                    month += 1
+                calview(calFile,month,year)
+            elif term.compKey(cmd,curses.KEY_DOWN,258): # Down arrow key
+                if month <= 1:
+                    month = 12
+                    year -= 1
+                else:
+                    month -= 1
+                calview(calFile,month,year)
+            elif term.compKey(cmd,curses.KEY_ENTER,10): # Enter key (Edit)
+                editEntry(item,month,year,events,calFile)
             else:
                 pass
             
@@ -92,29 +149,20 @@ def calview(calFile = "cal.txt",month = date.getStartingDate().split("-")[0], ye
         w.addstr("Leaving...")
         term.leave()
 
+
+# Yes, I know what you are thinking
+# "Why did you remove this?"
+# Well it's for presentation reasons
+# but you are welcome to add the code
+# back in.
 def calOpenPrompt():
-    try:
-        result = term.editablePrompt(dialog.cal.open)
-        
-        if result == "quit":
-            w.addstr("Aborting...")
-            term.leave()
-
-        if term.ynPrompt("Is this correct: " + str(result)) == False:
-            leave()
-
-        calview(result)
-    except KeyboardInterrupt:
-        w.addstr("\nAborting...\n")
-        term.leave()
-        sys.exit(1)
+    calview("cal.txt",int(date.getStartingDate().split("-")[0]))
     
-
 def main():
     try:
         clear = True
         while 1:
-            if clear == True and debug > 0:
+            if clear == True and debug < 1:
                 w.clear()
             term.print(dialog.main.intro)
             if calFile == "":
@@ -150,7 +198,7 @@ def main():
                     w.addstr("\nunctrl: " + str(truecmd))
                 else:
                     clear = False
-                    w.addstr(str("Invalid command"))
+                    w.addstr(str(" "))
             w.refresh()            
         
     except KeyboardInterrupt:
@@ -160,8 +208,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    
 else:
     print("Do not import this program")
     sys.exit(1)
